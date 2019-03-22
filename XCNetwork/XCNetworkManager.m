8d8c0f0042665845e2ece9d3e6187b1d8ce386f1
Tang杰 <tang910103@qq.com>
2019-03-22 22:36:42 +0800

    update

--- XCNetwork/XCNetworkManager.m
+++ XCNetwork/XCNetworkManager.m
@@ -6,10 +6,15 @@
 //
 
 #import "XCNetworkManager.h"
-#import <AFNetworking/AFNetworking.h>
 #import "XCBaseRequest.h"
 #import "XCNetworkConfig.h"
 #import "XCNetworkCache.h"
+#if __has_include(<AFNetworking/AFNetworking.h>)
+#import <AFNetworking/AFNetworking.h>
+#else
+#import "AFNetworking.h"
+#endif
+
 
 #define weakObj(self) __weak typeof(self) weak_##self = self;
 #define strongObj(self) __strong typeof(self) self = weak_##self;
